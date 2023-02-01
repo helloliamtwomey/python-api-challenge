35aed13ec96db380660db95cb07b495c02a998f2
Liam Twomey <hello@liamtwomey.com>
2023-02-01 14:11:26 +0800

    update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0410c548ea524c72ae010469997a3008"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDdwobAEUcWQSR_pGdYuDSqVCPxOSTq8ks"
